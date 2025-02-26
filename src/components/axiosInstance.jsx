a1fe57d45dbc67c820cf140cf5f757e792e46e1c
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2025-02-26 23:34:59 +0530

    commit

--- src/components/axiosInstance.jsx
+++ src/components/axiosInstance.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3500',
+  baseURL: 'https://acme-be.vercel.app/',
   headers: {
     'Content-Type': 'application/json',
   },
